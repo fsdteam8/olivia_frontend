56d48dc34eebaeeafea143905010402c40217b04
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-04-01 04:42:07 +0600

    feat: enroll course

--- src/app/courses/[id]/enroll-course/_components/enroll-course.tsx
+++ src/app/courses/[id]/enroll-course/_components/enroll-course.tsx
@@ -1,10 +1,17 @@
-import React from "react";
 import CourseWatchPage from "./course-watch-page";
+import WhatYouWillLearn from "../../_components/what-you-will-learn";
+import CourseBenefits from "../../_components/course-benefits";
+import ExploreOtherCourses from "../../_components/other-courses";
+import CommunitySection from "@/components/home/CommunitySection";
 
 const EnrollCourse = () => {
   return (
     <div className="space-y-16 lg:space-y-20 mt-16">
       <CourseWatchPage />
+      <WhatYouWillLearn />
+      <CourseBenefits />
+      <ExploreOtherCourses />
+      <CommunitySection />
     </div>
   );
 };
