068a90d96d196e982000fca205b0df4b7540cbc8
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-04-01 04:08:47 +0600

    feat: what you will learn section

--- src/app/courses/[id]/_components/course-details.tsx
+++ src/app/courses/[id]/_components/course-details.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FeaturedCourseSection from "./feature-course-section";
+import WhatYouWillLearn from "./what-you-will-learn";
 
 const CourseDetails = () => {
   return (
     <div className="space-y-16 lg:space-y-20 mt-16">
       <FeaturedCourseSection />
+      <WhatYouWillLearn />
     </div>
   );
 };
