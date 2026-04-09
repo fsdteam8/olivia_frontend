21379dba1b23ec01d4073e1d1d4a8be6f0a5c1af
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-04-09 21:39:48 +0600

    feat: payment success page done

---
+++ src/app/(website)/payment/success/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PaymentSuccessPage from "./_components/payment-success-page";
+
+const page = () => {
+  return (
+    <div className="mt-20">
+      <PaymentSuccessPage />
+    </div>
+  );
+};
+
+export default page;
