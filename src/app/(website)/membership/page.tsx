00a9f7ba49b0a4cc6a9ecb5c2d75cb9743aa7dc7
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-04-10 19:42:13 +0600

    feat: how it works added

--- src/app/(website)/membership/page.tsx
+++ src/app/(website)/membership/page.tsx
@@ -2,17 +2,21 @@ import CommunitySection from "@/components/home/CommunitySection";
 import { Newsletter } from "@/components/home/Newsletter";
 import PageHero from "@/components/home/PageHero";
 import React from "react";
+import HowItWorks from "./_components/how-it-works";
 
 const page = () => {
   return (
     <div className="space-y-16 lg:space-y-20">
-      <PageHero
-        bgImage="/membership.jpg"
-        subtitle="Act on Climate brings together students, early-career professionals, and experienced leaders from around the world who are committed to driving climate solutions. We are building a collaborative community for learning, connection, and real-world impact."
-        title="Join a Global Climate Community"
-        isHide={false}
-        buttonTitle="Become a Member"
-      />
+      <div>
+        <PageHero
+          bgImage="/membership.jpg"
+          subtitle="Act on Climate brings together students, early-career professionals, and experienced leaders from around the world who are committed to driving climate solutions. We are building a collaborative community for learning, connection, and real-world impact."
+          title="Join a Global Climate Community"
+          isHide={false}
+          buttonTitle="Become a Member"
+        />
+        <HowItWorks />
+      </div>
 
       <div>
         <Newsletter />
