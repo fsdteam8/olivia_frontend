fd4733cade5b3fbbd38eb9b36b62f5f222d894d4
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-04-10 16:46:07 +0600

    feat: climate career guide section done

--- src/app/(website)/find-your-opportunity/page.tsx
+++ src/app/(website)/find-your-opportunity/page.tsx
@@ -1,9 +1,13 @@
+import ClimateCareerGuide from "./_components/climate-career-guide";
 import PageHeader from "./_components/page-header";
 
 const page = () => {
   return (
     <div className="space-y-16 lg:space-y-20">
-      <PageHeader />
+      <div>
+        <PageHeader />
+        <ClimateCareerGuide />
+      </div>
     </div>
   );
 };
